ea39858d82cd17fb11d914044bee6678727d729c
Steven McDonald <steven.mcdonald@gmail.com>
2023-11-28 15:58:04 -0800

    some demo scripts

---
+++ public/test/list.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Play Store Monitor</title>
+</head>
+<body>
+    <div class="rows">
+        <table>
+            <tr>
+                <th>timestamp</th>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Country</th>
+                <th>Availbale</th>
+            <tr>
+<?php
+
+require '../../inc/psm.inc';
+
+global $mongodb_manager;
+
+$filter = [];
+$options = [
+    'limit' => 20,
+    'sort' => [
+        'ts' => -1
+    ]
+];
+
+$mongodb_query = new MongoDB\Driver\Query($filter, $options);
+$rows = $mongodb_manager->executeQuery('psm.main', $mongodb_query);
+
+foreach($rows as $row) {
+    // echo '######### ' . $row->request->id . "\n";
+    // var_dump($row->request);
+    // echo "\n";
+    // var_dump($row->response);
+?>
+        <tr>
+            <td><?php echo $row->ts ?></td>
+            <td><?php echo $row->request->id ?></td>
+            <td><?php echo $row->response->name ?></td>
+            <td><?php echo $row->request->gl ?></td>
+            <td><?php echo $row->response->available ? '✅' : '❌' ?></td>
+        </tr>
+
+<?php
+}
+
+?>
+        </table>
+    </div>
+</body>
+</html>
---
+++ public/test/search.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Play Store Monitor</title>
+</head>
+<body>
+<?php
+
+require '../../inc/psm.inc';
+
+?>
+<h2>Play Store Montior</h2>
+<div>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        <label for="q">Search:</label>
+        <input type="text" name="q">
+        <button type="submit">Search</button>
+    </form>
+</div>
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $term = $_POST['q'];
+
+    $ids = search_play_store($term, 'us');
+
+?>
+    <table>
+        <tr>
+            <th>App</th>
+            <th>US</th>
+            <th>CN</th>
+        </tr>
+<?php
+    foreach($ids as $id) {
+        ?><tr><?php
+
+        $us_response = test_app($id, 'us');
+        $cn_response = test_app($id, 'cn');
+
+        $name = $us_response->name;
+
+        $us_avail = ($us_response->available == true) ? '✅' : '❌';
+        $cn_avail = ($cn_response->available == true) ? '✅' : '❌';
+        echo "<td>$name</td><td>$us_avail</td><td>$cn_avail</td>\n";
+
+        ?></tr><?php
+    }
+}
+
+?>
+</body>
+</html>
