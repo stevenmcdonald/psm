d5fe0ed9ecf029d3c9511a119fe3d8f88366672c
Steven McDonald <steven.mcdonald@gmail.com>
2023-11-30 13:33:23 -0800

    move this to a template so I dont' check in the password again ;-)

--- scripts/recreate_status_changes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-php recreate_status_changes.php
-
-# mongosh mongodb://psm@localhost/psm -p fuwo1yeed0aekaizi2OoY4eiriequait --eval "db.status_changes_tmp.ensureIndex({ts: -1})"
-# mongosh mongodb://psm@localhost/psm -p fuwo1yeed0aekaizi2OoY4eiriequait --eval "db.status_changes_tmp.renameCollection('status_changes', true)"
