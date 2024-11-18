50f15c32f1cb26253d83df3f5578c9432c571ce7
Steven McDonald <steven.mcdonald@gmail.com>
2024-11-18 13:03:18 -0800

    sort the documents so $last has the right meaning

--- mongodb-scripts/03-app_names-min-full.js
+++ mongodb-scripts/03-app_names-min-full.js
@@ -1,6 +1,11 @@
 // main -> app_names
 recreate(db.main, [
     {
+        $sort: {
+            ts: 1
+        }
+    },
+    {
         $project: {
             territory: '$request.gl',
             id: '$request.id',
